833fda40b91ef8073774517e3f185705c7a51189
BANDARI VAMSHIKRISHNA <krishnavamshi.12321@gmail.com>
2022-08-25 11:21:05 +0530

    updating flask

--- index.py
+++ index.py
@@ -1,1 +1,12 @@
-print('Deployment done successfully')
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Deployment success'
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port= 5000, debug = True)
